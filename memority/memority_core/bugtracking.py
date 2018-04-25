40ff46e983f9823f1dddec91a58c8b611f9f1b5d
AndriyVityk <andrii.vityk@gmail.com>
2018-04-25 17:03:31 +0300

    fix import before decrypting

--- memority/memority_core/bugtracking.py
+++ memority/memority_core/bugtracking.py
@@ -2,15 +2,12 @@ import os
 
 import raven
 
-from settings import settings
-
 raven_client = raven.Client(
     dsn='https://7f943c9a0edc4af688fab169e0cac527:0c84e4329d044d589763e6c852a14d84@sentry.io/306286',
 
     # inform the client which parts of code are yours
     # include_paths=['my.app']
     include_paths=[os.path.dirname(__file__)],
-    name=f'{settings.address}',
     ignore_exceptions=[
         KeyboardInterrupt,
         NotImplementedError,
