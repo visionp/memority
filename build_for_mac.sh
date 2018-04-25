d8d399105f893a25fc5cb26cef43de3160bb4551
AndriyVityk <andrii.vityk@gmail.com>
2018-04-25 12:48:57 +0300

    Update build_for_mac.sh

--- build_for_mac.sh
+++ build_for_mac.sh
@@ -2,21 +2,31 @@
 
 . env/bin/activate
 
+echo "--------------------------------------------------"
+echo "Remove dirs"
 rm -rf build dist
 
-pyinstaller ./memority/gui/memority_gui.pyw --windowed --icon=img/icon.icns
+echo "--------------------------------------------------"
+echo "Compile GUI"
+pyinstaller ./memority/gui/memority_gui.pyw --windowed --icon=img/memority_icon_256.icns
 
-pyinstaller ./memority/memority_core/memority_core_systray.pyw --hidden-import cytoolz.utils --hidden-import cytoolz._signatures --hidden-import raven.handlers --hidden-import raven.handlers.logging --hidden-import sqlalchemy.ext.baked --additional-hooks-dir=pyinstaller-hooks --windowed --icon=img/icon.icns
+echo "--------------------------------------------------"
+echo "Compile Core"
+pyinstaller ./memority/memority_core/memority_core_systray.pyw --hidden-import cytoolz.utils --hidden-import cytoolz._signatures --hidden-import raven.handlers --hidden-import raven.handlers.logging --hidden-import sqlalchemy.ext.baked --additional-hooks-dir=pyinstaller-hooks --windowed --icon=img/memority_icon_256.icns
 
-mkdir dist/memority_gui/settings
-cp memority/gui/settings/defaults.yml dist/memority_gui/settings
+echo "--------------------------------------------------"
+echo "Add files to build"
+mkdir dist/memority_gui.app/Contents/MacOS/settings
+cp memority/gui/settings/defaults.yml dist/memority_gui.app/Contents/MacOS/settings
 
-mkdir dist/memority_core_systray/settings
-mkdir dist/memority_core_systray/smart_contracts
-mkdir dist/memority_core_systray/geth
-cp img/icon.ico dist/memority_core_systray
-cp memority/memority_core/settings/defaults.yml dist/memority_core_systray/settings
-cp -r memority/memority_core/smart_contracts/binaries dist/memority_core_systray/smart_contracts
-cp -r memority/memority_core/smart_contracts/binaries dist/memority_core_systray/smart_contracts
-cp -r memority/memority_core/smart_contracts/install dist/memority_core_systray/smart_contracts
-cp memority/memority_core/geth/darwin/geth dist/memority_core_systray/geth
+mkdir dist/memority_core_systray.app/Contents/MacOS/settings
+mkdir dist/memority_core_systray.app/Contents/MacOS/smart_contracts
+mkdir dist/memority_core_systray.app/Contents/MacOS/geth
+cp img/icon.ico dist/memority_core_systray.app/Contents/MacOS
+cp memority/memority_core/settings/defaults.yml dist/memority_core_systray.app/Contents/MacOS/settings
+cp -r memority/memority_core/smart_contracts/binaries dist/memority_core_systray.app/Contents/MacOS/smart_contracts
+cp -r memority/memority_core/smart_contracts/binaries dist/memority_core_systray.app/Contents/MacOS/smart_contracts
+cp -r memority/memority_core/smart_contracts/install dist/memority_core_systray.app/Contents/MacOS/smart_contracts
+cp memority/memority_core/geth/darwin/geth dist/memority_core_systray.app/Contents/MacOS/geth
+
+echo "Done!"
